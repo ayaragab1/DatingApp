c5308aa22c7e3a01f83161d4415d959265fd8103
Aya Ragab <ayaragab1699@gmail.com>
2022-05-31 00:23:13 +0200

    first commit

---
+++ DatingApp/Controllers/ValueController.cs
@@ -0,0 +1,29 @@
+﻿using DatingApp.Date;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    public class ValueController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ValueController( DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("Api/Value/GetValues")]
+        public async Task<IActionResult> GetValues()
+        {
+            var values =await _context.Values.ToListAsync();
+            return Ok(values); 
+        }
+
+        [HttpGet("Api/Value/{id}/GetValue")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(i=>i.Id==id);
+            return Ok(value);
+        }
+    }
+}
---
+++ DatingApp/Date/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Date;
+
+public class DataContext :DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) :base(options) {}
+    public DbSet<Value> Values { get; set; }
+}
